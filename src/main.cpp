5fd59ffc2231a2065e5ce0fc0b7321d90ab44567
Mathieu Trudelle <73758838+trudelle200902@users.noreply.github.com>
2025-02-03 15:47:02 -0500

    #logo marche ca flash

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BUFF_SIZE (6)
+
+char buff[BUFF_SIZE] = {0};
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial1.begin(115200);
+
+  // while (!Serial1.available() || !Serial.available()){}
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Serial.println("#LOGO");
+  Serial1.println("#LOGO");
+  Serial1.read(buff, BUFF_SIZE);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for (uint8_t i = 0; i < BUFF_SIZE; i++) {
+    Serial.print(buff[i]);
+  }
+  Serial.println("");
+
+  delay(1500);
+  
+  // put your main code here, to run repeatedly:
 }
